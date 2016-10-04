4a62b3db6e0797d6b399fe49eec4854b30c1c62b
Filipe <filipemateusn@gmail.com>
2016-10-04 00:16:43 -0300

    adding class SpeedTester

---
+++ tester.js
@@ -0,0 +1,21 @@
+"use strict"
+
+class SpeedTest {
+  constructor(implementation, loops) {
+    this.average = 0;
+    this.implementation = implementation;
+    this.loops = loops || 1000;
+  }
+
+  start() {
+    let beginTime, endTime, sumTimes = 0;
+    for (let i = 0, x = this.loops; i < x; i++) {
+      beginTime = +new Date();
+      this.implementation();
+      endTime = +new Date();
+      sumTimes = endTime - beginTime;
+    }
+    this.average = sumTimes / this.loops;
+    return console.log(`Average execution across ${this.loops}: ${this.average}`);
+  }
+}
